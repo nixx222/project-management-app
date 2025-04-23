75c5fa158e7708614b9d03bd478a56ef703234ba
nixx222 <167340686+nixx222@users.noreply.github.com>
2025-04-23 12:29:09 -0700

    added boiler plate server code to index.js file.

--- server-backend/src/index.js
+++ server-backend/src/index.js
@@ -1,8 +1,26 @@
+import express from "express"; //external module for using express
+import pg from "pg";
+const { Client } = pg;
+
 let config = {
-	user: process.env.USER,
-	host: process.env.HOST,
-	database: process.env.DATABASE,
-	password: process.env.PASSWORD,
-	port: process.env.DATABASE_PORT,
-	ssl: true
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DATABASE_PORT,
+  ssl: true,
 };
+//boiler plate express code which allows us to run our web server
+const app = express();
+const port = 3000;
+
+//we're using json as our data format
+app.use(express.json());
+
+//config is saying "this is how we connect to our database", and now I need tools.
+
+//Always connect to the database, give the code, then close the database or else it will time out.
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
